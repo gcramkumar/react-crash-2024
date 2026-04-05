fce19b2183ad8021b1917488c44c026207da1cbb
gcramkumar <gcramkumar@gmail.com>
2026-04-05 12:15:10 +0200

    second commit message line

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,14 @@ import { Route,
  } from "react-router-dom";
  import HomePage from "./pages/HomePage";
  import MainLayout from "./layouts/MainLayout";
+ import JobsPage from "./pages/JobsPage";
 
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route element={<MainLayout />}>
       <Route index element={<HomePage />} />
+      <Route path='/jobs' element={<JobsPage />} />
     </Route>
   )
 );
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,9 +1,16 @@
 import Hero from "../components/Hero"
+import HomeCards from "../components/HomeCards"
+import JobListings from "../components/JobListings"
+import ViewAllJobs from "../components/ViewAllJobs"
+
 
 const HomePage = () => {
   return (
     <div>
         <Hero />
+        <HomeCards />
+        <JobListings />
+        <ViewAllJobs />
     </div>
   )
 }
