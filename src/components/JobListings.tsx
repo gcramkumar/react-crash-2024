5cd1c7f00eb9eb3bd8e21e36ccc3c3c6fa359c6a
gcramkumar <gcramkumar@gmail.com>
2026-04-05 20:01:46 +0200

    commit 4

--- src/components/JobListings.tsx
+++ src/components/JobListings.tsx
@@ -1,10 +1,43 @@
-import jobs from '../jobs.json'
+import { useState, useEffect } from 'react';
 import JobListing from './JobListing';
+import  Spinner from './Spinner';
 
+interface job_type {
+  id: string;
+  title: string;
+  type: string;
+  description: string;
+  location: string;
+  salary: string;
+  company: {
+    name: string;
+    description: string;
+    contactEmail: string;
+    contactPhone: string;
+  };
+}
 
 const JobListings = ({isHome=false}) => {
 
-const JobListings = isHome ?jobs.slice(0, 3) : jobs  ;
+const [jobs, setJobs] = useState<job_type[]>([]);
+const [loading, setLoading] = useState(true);
+
+useEffect(() => {
+   const fetchJobs = async () => {
+    const apiUrl = isHome ? '/api/jobs?_limit=3' : '/api/jobs';
+    try {
+      const response = await fetch(apiUrl);
+      const data = await response.json();
+      const jobsData: job_type[] = Array.isArray(data) ? data : data.jobs ?? [];
+      setJobs(jobsData);
+    } catch (error) {
+      console.error('Error fetching jobs:', error);
+    } finally {
+      setLoading(false);
+    }
+   }
+   fetchJobs();
+} , []);
 
 return (
 <section className="bg-blue-50 px-4 py-10">
@@ -12,11 +45,17 @@ return (
         <h2 className="text-3xl font-bold text-indigo-500 mb-6 text-center">
           {isHome ? 'Recent' : 'Browse'} Jobs
         </h2>
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-            {JobListings.map((job: any) => (
-                <JobListing key={job.id} job={job} />
+
+          {loading ? (
+            <Spinner loading={loading}  />
+          ) : (
+            <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+            {jobs.map((job:job_type) => (
+              <JobListing key={job.id} job={job} />
             ))}
-        </div>
+            </div>
+          )}
+
       </div>
     </section>
   )
